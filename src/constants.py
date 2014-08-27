0f0bb979cbb07503cc16d5405a1dddc8cfb9c6b8
Ygge95 <Ygge95@gmail.com>
2014-08-27 11:22:50 +0200

    Added new block_group class

--- src/constants.py
+++ src/constants.py
@@ -7,8 +7,8 @@ FPS = 60
 
 #Colours     R    G    B
 colours = {
-"BLACK" : (  0,   0,   0),
-"WHITE" : (255, 255, 255),
-"GREEN" : (  0, 255,   0),
-"RANDOM": (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
+    "BLACK" : (  0,   0,   0),
+    "WHITE" : (255, 255, 255),
+    "GREEN" : (  0, 255,   0),
+    "RANDOM": (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
 

c587f97450ad885fb44462a6e3aca1641da3e3b2
Brage Bunk <bragesb@gmail.com>
2014-08-28 13:44:58 +0200

    Fixed movement for block groups

--- src/game.py
+++ src/game.py
@@ -44,12 +44,15 @@ while True:
             if event.key == K_RIGHT and check_right_collision(current_blocks, screen_rect):
                 clear = True
                 for block in blocks:
-                    if block.rect.top < current_block.rect.bottom and block.rect.bottom > current_block.rect.top and block.rect.left == current_block.rect.right:
-                        clear = False
-                        break
-
+                    for current_block in current_blocks:
+                        if block.rect.top < current_block.rect.bottom and block.rect.bottom > current_block.rect.top and block.rect.left == current_block.rect.right:
+                            clear = False
+                            break
+                if not clear:
+                    break
                 if clear:
-                    current_block.rect.left = current_block.rect.right
+                    for current_block in current_blocks:
+                        current_block.rect.left = current_block.rect.right
 
             if event.key == K_LEFT and check_left_collision(current_blocks, screen_rect):
                 clear = True
@@ -71,22 +74,34 @@ while True:
 
     pygame.display.set_caption("FPS: %d" % clock.get_fps())
     pygame.display.update()
+
+    current_block_stopped = False
+    distance_to_raise = 0
+
     for current_block in current_blocks:
         current_block.update()
 
         if current_block.rect.bottom >= screen_rect.bottom:
-            current_block.speed = 0
-            current_block.rect.bottom = screen_rect.bottom
-            blocks.append(current_block)
-            current_block = Blockgroup().block_group
+            distance_to_raise = screen_rect.bottom - current_block.rect.bottom
+            current_block_stopped = True
+            break
 
-    for block in blocks:
+    if not current_block_stopped:
+        for block in blocks:
+            for current_block in current_blocks:
+                if current_block.rect.bottom >= block.rect.top and current_block.rect.centerx == block.rect.centerx:
+                    distance_to_raise = block.rect.top - current_block.rect.bottom
+                    current_block_stopped = True
+                    break
+            if current_block_stopped:
+                break
+
+    if current_block_stopped:
         for current_block in current_blocks:
-            if current_block.rect.bottom >= block.rect.top and current_block.rect.centerx == block.rect.centerx:
-                current_block.speed = 0
-                current_block.rect.bottom = block.rect.top
-                blocks.append(current_block)
-                current_block = Blockgroup().block_group
+            current_block.speed = 0
+            current_block.rect.bottom += distance_to_raise
+            blocks.append(current_block)
+            current_blocks = Blockgroup().block_group
 
 
 
